aa3b38369936f7b99db1ef093f3b341b5d8f5be7
amirmohazzab <ahm.mohazzab@gmail.com>
2022-09-17 09:06:40 -0700

    second deploy my app

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,10 @@ import thunk from "redux-thunk";
 
 export const store = createStore(
     reducers,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    compose(applyMiddleware(thunk))
 );
 
 //subscribe
 store.subscribe(() => console.log(store.getState()));
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
